3710944156ff2d956ca829beb65fe43e5ca24b41
michelml <michmoreau.l@gmail.com>
2019-10-14 15:27:37 -0400

    edit app to see changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Amplify
         </a>
       </header>
     </div>
